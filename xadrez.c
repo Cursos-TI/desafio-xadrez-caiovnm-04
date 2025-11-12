b5248766745d7c4c183655e880ada279314b5d74
caiovnm-04 <cvnmagalhaes@gmail.com>
2025-11-12 22:31:18 +0000

    xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,51 +1,67 @@
 #include <stdio.h>
 
+void movi_torre(int cassas)
+{
+    if (cassas > 0)
+    {
+        printf("Direita\n");
+        movi_torre(cassas - 1);
+    }
+}
+
+void movi_bispo(int cassas)
+{
+    if (cassas > 0)
+    {
+        printf("Cima Direita\n");
+        movi_bispo(cassas - 1);
+    }
+}
+
+void movi_rainha(int cassas)
+{
+    if (cassas > 0)
+    {
+        printf("Esquerda\n");
+        movi_rainha(cassas - 1);
+    }
+}
+
+void movi_cavalo(int cassas)
+{
+    for (int i = 0; i < cassas; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("Cima\n");
+        }
+
+        printf("Direita");
+    }
+}
+
 int main()
 {
     // Movimentos da Torre (movendo 5 casas para a direita)
     printf("Movimento da torre:\n");
-
-    for (int i = 1; i <= 5; i++)
-    {
-        printf("Direita\n");
-    }
+    movi_torre(5);
 
     printf("\n");
 
     // Movimentos do Bispo (movendo 5 casas na diagonal para cima e à direita)
-    int bispo = 1;
+
     printf("Movimentos do bispo:\n");
-    while (bispo <= 5)
-    {
-        printf("Cima Direita\n");
-        bispo++;
-    }
+    movi_bispo(5);
 
     printf("\n");
 
-    int rainha = 1;
     printf("Movimento da Rainha\n");
-    do
-    {
-        printf("Esquerda\n");
-        rainha++;
-    } while (rainha <= 8);
+    movi_rainha(8);
 
-     printf("\n");
-
-    int cavalo = 0;
+    printf("\n");
 
     printf("Movimento cavalo \n");
-    do
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            printf("baixo\n");
-        }
-            printf("Esquerda\n");
-            cavalo++;
-
-    } while (cavalo < 1);
+    movi_cavalo(1);
 
     return 0;
 }
